3e3413290b99f23062c88499a954613ea5765059
Myeongjin <aranet100@gmail.com>
2013-12-25 20:25:13 +0900

    update message and add translation

---
+++ lang/ko/settings.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Korean language file
+ *
+ * @author Myeongjin <aranet100@gmail.com>
+ */
+
+$lang['path'] = '로컬 graphviz dot 바이너리로의 경로 (예를 들어 <code>/usr/bin/dot</code>). google.com에서 원격 렌더하기를 사용하려면 비워 두세요.';
