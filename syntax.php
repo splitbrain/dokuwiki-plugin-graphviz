ddf7563854d02a442d34a2bf3db3fb8727fc84b8
Ole Schuett <ole@schuett.name>
2015-01-25 00:59:18 +0100

    Add support for URLs and tooltips.

--- syntax.php
+++ syntax.php
@@ -105,7 +105,10 @@ class syntax_plugin_graphviz extends DokuWiki_Syntax_Plugin {
             if($data['height']) $R->doc .= ' height="'.$data['height'].'"';
             if($data['align'] == 'right') $R->doc .= ' align="right"';
             if($data['align'] == 'left')  $R->doc .= ' align="left"';
-            $R->doc .= '/>';
+            $R->doc .= ' usemap="#graphviz_'.$data['md5'].'"';
+            $R->doc .= "/>\n";
+            $R->doc .= io_readFile($this->_imgmap($data), false);
+            $R->doc .= '<script type="text/javascript">/*<![CDATA[*/ imageMapResize(); /*!]]>*/</script>'."\n";
             return true;
         }elseif($format == 'odt'){
             $src = $this->_imgfile($data);
@@ -123,19 +126,8 @@ class syntax_plugin_graphviz extends DokuWiki_Syntax_Plugin {
 
         // create the file if needed
         if(!file_exists($cache)){
-            $in = $this->_cachename($data,'txt');
-            if($this->getConf('path')){
-                $ok = $this->_run($data,$in,$cache);
-            }else{
-                $ok = $this->_remote($data,$in,$cache);
-            }
+            $ok = $this->_run($data);
             if(!$ok) return false;
-            clearstatcache();
-        }
-
-        // resized version
-        if($data['width']){
-            $cache = media_resize_image($cache,'png',$data['width'],$data['height']);
         }
 
         // something went wrong, we're missing the file
@@ -145,9 +137,44 @@ class syntax_plugin_graphviz extends DokuWiki_Syntax_Plugin {
     }
 
     /**
+     * Return path to the rendered map on our local system
+     */
+    function _imgmap($data){
+        $cache  = $this->_cachename($data,'map');
+
+        // create the file if needed
+        if(!file_exists($cache)){
+            $ok = $this->_run($data);
+            if(!$ok) return false;
+        }
+
+        // something went wrong, we're missing the file
+        if(!file_exists($cache)) return false;
+
+        return $cache;
+    }
+
+    /**
+     * Render the graph
+     */
+    function _run($data){
+        $cache_png  = $this->_cachename($data,'png');
+        $cache_map  = $this->_cachename($data,'map');
+        $in = $this->_cachename($data,'txt');
+        if($this->getConf('path')){
+            $ok = $this->_run_local($data,$in,$cache_png,$cache_map);
+        }else{
+            $ok = $this->_run_remote($data,$in,$cache_png,$cache_map);
+        }
+        if(!$ok) return false;
+        clearstatcache();
+        return true;
+    }
+
+    /**
      * Render the output remotely at google
      */
-    function _remote($data,$in,$out){
+    function _run_remote($data,$in,$out_png,$out_map){
         if(!file_exists($in)){
             if($conf['debug']){
                 dbglog($in,'no such graphviz input file');
@@ -165,13 +192,16 @@ class syntax_plugin_graphviz extends DokuWiki_Syntax_Plugin {
         $img = $http->post('http://chart.apis.google.com/chart',$pass,'&');
         if(!$img) return false;
 
-        return io_saveFile($out,$img);
+        //Unfortunately, google chart doesn't support image-maps, store an empty map.
+        $ok = io_saveFile($out_map, '<map name="graphviz_'.$data['md5'].'"></map>');
+        if(!$ok) return false;
+        return io_saveFile($out_png,$img);
     }
 
     /**
-     * Run the graphviz program
+     * Run the graphviz program locally
      */
-    function _run($data,$in,$out) {
+    function _run_local($data,$in,$out_png,$out_map) {
         global $conf;
 
         if(!file_exists($in)){
@@ -182,9 +212,11 @@ class syntax_plugin_graphviz extends DokuWiki_Syntax_Plugin {
         }
 
         $cmd  = $this->getConf('path');
-        $cmd .= ' -Tpng';
         $cmd .= ' -K'.$data['layout'];
-        $cmd .= ' -o'.escapeshellarg($out); //output
+        $cmd .= ' -Tpng';
+        $cmd .= ' -o'.escapeshellarg($out_png); //output png image
+        $cmd .= ' -Tcmapx';
+        $cmd .= ' -o'.escapeshellarg($out_map); //output image map
         $cmd .= ' '.escapeshellarg($in); //input
 
         exec($cmd, $output, $error);
@@ -195,7 +227,11 @@ class syntax_plugin_graphviz extends DokuWiki_Syntax_Plugin {
             }
             return false;
         }
-        return true;
+
+        // change name of image-map
+        $map = io_readFile($out_map,false);
+        $map = preg_replace('/<map [^>]*>/', '<map name="graphviz_'.$data['md5'].'">', $map);
+        return io_saveFile($out_map,$map);
     }
 
 }
