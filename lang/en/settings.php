a7e428d17237b185cdd0430b3c3d9587bf9954e9
psy <psy@darmstadt.ccc.de>
2022-01-15 14:54:25 +0100

    add language string

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -1,3 +1,4 @@
 <?php
 
 $lang['path'] = 'The path to your local graphviz dot binary (eg. <code>/usr/bin/dot</code>). Leave empty to use remote rendering at google.com.';
+$lang['google_fallback'] = 'Use google.com as fallback to generate images if no local installation is found.';
